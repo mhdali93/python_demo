0572b4c81cb6d540d52898a48b93b39ae754304a
mhd <m.mhdali91@gmail.com>
2025-03-28 00:41:36 +0530

    execution time and logging decorator, standarisation of response

--- flask/main.py
+++ flask/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from utils import Utils
+import time
 
 app = Flask(__name__)
 
@@ -12,22 +13,34 @@ def root():
 
 @app.route("/listItem")
 def list_items():
-    return jsonify(items)
+    try:
+        return jsonify(items)
+    except Exception as e:
+        raise(e)
 
 @app.route("/addItem", methods=['POST'])
+@Utils.logging_dec
 @Utils.profiling_dec
 def create_item():
-    reqObj = request.json
-    item = {
-        "id": len(items)+1,
-        "name": reqObj["name"] if "name" in reqObj else "",
-        "desc": reqObj["desc"] if "desc" in reqObj else "",
-        "price": reqObj["price"] if "price" in reqObj else 0
-    }
-    # Utils.hi_print(f"Item to be added: {item}")
-    items.append(item)
-    # Utils.hi_print(f"final list after adding item: {items}")
-    return dict(item)
+    try:
+        reqObj = request.json
+        item = {
+            "id": len(items)+1,
+            "name": reqObj["name"] if "name" in reqObj else "",
+            "desc": reqObj["desc"] if "desc" in reqObj else "",
+            "price": reqObj["price"] if "price" in reqObj else 0
+        }
+        items.append(item)
+        g = [[f"{x} x {y+1} = {x * (y+1)}" for y in range(10)] for x in range(50000)]
+        
+        time.sleep(1)
+
+        print(g[1])
+        print(items)
+        return jsonify(item)
+    except Exception as e:
+        raise(e)
+
 
 
 if __name__ == "__main__": 
--- flask/utils.py
+++ flask/utils.py
@@ -1,12 +1,14 @@
+from functools import wraps
+import json
 import time
 from datetime import datetime
 
 class Utils:
     def __highlighted_print(func):
         def wrapper(*args, **kwargs):
-            print(f"\n {'-'*100} \n")
+            print(f"\n {'-'*100}")
             func(*args, **kwargs)
-            print(f"\n {'-'*100} \n")
+            print(f"{'-'*100} \n")
         return wrapper
     
     @staticmethod
@@ -17,14 +19,31 @@ class Utils:
 
     @staticmethod
     def profiling_dec(func):
+        @wraps(func)
         def wrapper(*args, **kwargs):
-            Utils.hi_print(f"Start time {func.__name__}")
+            st_time = time.time()
+            st_dt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             result = func(*args, **kwargs)
-            Utils.hi_print(f"End time {func.__name__}")
-            return result
+            en_time = time.time()
+            en_dt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            response_data = {
+                "request_time": st_dt,
+                "response_time": en_dt,
+                "excecution_time": en_time - st_time,
+                "data": json.loads(result.get_data(as_text=True))
+            }
+            Utils.hi_print(f"End time {func.__name__}, excecution time: {en_time - st_time}")
+            return response_data
         return wrapper
 
-
+    @staticmethod
+    def logging_dec(func):
+        def wrapper(*args, **kwargs):
+            Utils.hi_print(f"{str(*args)},{str(**kwargs)}")
+            result = func(*args,**kwargs)
+            Utils.hi_print(str(result))
+            return result
+        return wrapper
 
 
 
